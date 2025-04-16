6e6f4723e4d410af04462338575e9a243d38c99d
Pierre-Henry Lesage <PHL@analis.local>
2025-04-16 11:55:50 +0200

    comment deleted

--- Domain/Interfaces/User/IUserRepository.cs
+++ Domain/Interfaces/User/IUserRepository.cs
@@ -16,7 +16,5 @@ namespace Domain_Layer.Interfaces.User
         Task<Entities.User> UpdateUserAsync(Entities.User user);
         Task<bool> DeleteUserAsync(int id);
 
-        //TODO : Add a ContextSaveChange when we need to change alot of stuff and Generic Interface
-
     }
 }
